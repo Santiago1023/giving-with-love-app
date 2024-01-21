7cf9db38c851b119a66dc52f6a39ab19e0875d65
santiago1023 <santiago.bedoyad@udea.edu.co>
2024-01-21 13:12:59 -0500

    Carosuel component close to finish

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
 // import { NavbarUI } from "@/components/Navbar/NavbarUI";
 
+import { SliderPhotos } from "@/components/Slider";
+
 // import { Ensayo } from "@/components/ensayo";
 
 const Home = () => {
   return (
     <main className="flex flex-col gap-5">
-      <span className="text-white">HomePage</span>
-      {/* <NavbarUI/> */}
-      {/* <Ensayo/> */}
+      <SliderPhotos/>
     </main>
   )
 }
